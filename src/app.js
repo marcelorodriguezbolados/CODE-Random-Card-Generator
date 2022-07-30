de07f309ff5c8db9796dfa03ecbcb09676f30573
marcelorodriguezbolados <marcelofabianrb@gmail.com>
2022-07-30 01:47:02 +0000

    code listo carta

--- src/app.js
+++ src/app.js
@@ -7,5 +7,58 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //llamando html
+  let card = document.querySelector(".card");
+  let up = document.querySelector(".up-suit");
+  let nu = document.querySelector(".number");
+  let down = document.querySelector(".down-suit");
+
+  let pintitas = RandomPinta();
+
+  //condicion color
+
+  if (pintitas == "♥" || pintitas == "♦") {
+    up.style.color = "red";
+  }
+
+  if (pintitas == "♥" || pintitas == "♦") {
+    down.style.color = "red";
+  }
+
+  //insertar en html
+
+  nu.innerHTML = RandomNumber();
+  up.innerHTML = pintitas;
+  down.innerHTML = pintitas;
+
+  //document.querySelector(".card").innerHTML();
+};
+
+let RandomNumber = function() {
+  let Numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let num = Math.floor(Math.random() * Numbers.length);
+
+  return Numbers[num];
+};
+
+let RandomPinta = function() {
+  let pintacartas = ["♦", "♥", "♠", "♣"];
+  let eleccionpintas = Math.floor(Math.random() * pintacartas.length);
+
+  return pintacartas[eleccionpintas];
 };
